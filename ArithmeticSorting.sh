b4b5b200392f632372ed34e1aedff962f6928392
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-08-05 08:21:38 +0530

    comput_a+b*c.sh

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -10,3 +10,15 @@ echo ""
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
+
+#UC2
+
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
+
+#COMPUTE ARITHMETIC OPERATION
+result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result
+
