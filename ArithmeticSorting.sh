bde25f7de4ec01ba4ca54e3d372e97ce4057eaf3
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-08-05 09:04:50 +0530

    store_in _directionry for every computation.sh

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -59,3 +59,31 @@ read -p "Enter third input :" thirdInput
 resultThree=`echo "scale=2;$firstInput % $secondInput + $thirdInput" | bc`
 echo "Result:" $resultThree
 
+#UC6
+
+#TO DECLEAR THE DICTIONARY
+declare -A arithmaticOperation
+
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
+
+#COMPUTE ARITHMETIC OPERATION
+result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result
+
+resultOne=`echo "scale=2;$firstInput * $secondInput + $thirdInput" | bc`
+echo "Result:" $resultOne
+
+resultTwo=`echo "scale=2;$thirdInput + $firstInput / $secondInput " | bc`
+echo "Result:" $resultTwo
+
+resultThree=`echo "scale=2;$firstInput % $secondInput + $thirdInput" | bc`
+echo "Result:" $resultThree
+
+#TO STORE THE DICTIONARY
+arithmaticOperation[result]=$result
+arithmaticOperation[resultOne]=$resultOne
+arithmaticOperation[resultTwo]=$resultTwo
+arithmaticOperation[resultThree]=$resultThree
